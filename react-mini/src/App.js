f55506a8da46920f8817ce627a8c6e2cacf5b1ae
rk1091 <riya_kapoor@hotmail.com>
2022-12-28 11:59:57 +0530

    register login done
    moving on to main app components

--- react-mini/src/App.js
+++ react-mini/src/App.js
@@ -1,7 +1,10 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
+import "./style.scss";
 
 function App() {
-  return <Register />;
+  return <Home />;
 }
 
 export default App;
--- react-mini/src/pages/Register.jsx
+++ react-mini/src/pages/Register.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import Add from "../img/addAvatar.png";
 
 const Register = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Register</span>
         <form>
-          <input type="text" />
-          <input type="email" />
-          <input type="password" />
+          <input type="text" placeholder="text" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an Avatar</span>
+          </label>
+          <button>Sign up</button>
         </form>
+        <p>Already have an account? Login</p>
       </div>
     </div>
   );
