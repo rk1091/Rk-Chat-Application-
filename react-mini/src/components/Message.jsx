dcf4423f9fb550544e31c50e401f9efd027c3190
rk1091 <riya_kapoor@hotmail.com>
2022-12-30 02:52:46 +0530

    react frontend done
    all components complete
    next is backend ie starting with firebase

--- react-mini/src/components/Message.jsx
+++ react-mini/src/components/Message.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import Add from "../img/add.jpeg";
 const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="messageInfo">
         <img src={Add} alt="" />
         <span>just now</span>
       </div>
-      <div className="messageContent"></div>
-      <p>hello</p>
-      <img src={Add} alt="" />
+      <div className="messageContent">
+        <p>hello</p>
+        <img src={Add} alt="" />
+      </div>
     </div>
   );
 };
