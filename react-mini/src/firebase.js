17793c795a8d3a78a9d0e6df7d3bb9b6681fc1bd
rk1091 <riya_kapoor@hotmail.com>
2022-12-30 12:18:04 +0530

    started with auth

---
+++ react-mini/src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRSWAfwFAE1fdj2fvM8Npt12pquloSQus",
+  authDomain: "rk-chat-99148.firebaseapp.com",
+  projectId: "rk-chat-99148",
+  storageBucket: "rk-chat-99148.appspot.com",
+  messagingSenderId: "772466433939",
+  appId: "1:772466433939:web:ee6688f705619b232c5266",
+  measurementId: "G-GF1N056DHZ",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const analytics = getAnalytics(app);
--- react-mini/src/pages/Register.jsx
+++ react-mini/src/pages/Register.jsx
@@ -1,13 +1,43 @@
 import React from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import Add from "../img/addAvatar.png";
-
+import { auth } from "../firebase";
+import { useState } from "react";
 const Register = () => {
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      setErr(true);
+    }
+
+    // createUserWithEmailAndPassword(auth, email, password)
+    // .then((userCredential) => {
+    //   // Signed in
+    //   const user = userCredential.user;
+    //   console.log(user);
+    //   // ...
+    // })
+    // .catch((error) => {
+    //   const errorCode = error.code;
+    //   const errorMessage = error.message;
+    //   // ..
+    // });
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Rk's Chat</span>
         <span className="title">Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="text" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
@@ -17,6 +47,7 @@ const Register = () => {
             <span>Add an Avatar</span>
           </label>
           <button>Sign up</button>
+          {err && <span>Something went wrong </span>}
         </form>
         <p>Already have an account? Login</p>
       </div>
